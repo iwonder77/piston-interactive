83972e9b7b0c21659fc5ed1301356dc616153dd8
Isai Sanchez <isanchez@thanksgivingpoint.org>
2026-08-13 15:53:44 -0600

    fix: removed unecessary I2C timeout

--- torque-hp-led-driver/src/Config.h
+++ torque-hp-led-driver/src/Config.h
@@ -8,8 +8,7 @@
  */
 #include <Arduino.h>
 namespace config {
-// --- WATCHDOG & BUS TIMEOUTS ---
+// --- WATCHDOG ---
 constexpr uint32_t WDT_TIMEOUT_MS = 5000;
 constexpr uint32_t WDT_IDLE_CORE_MASK = (1 << 0);
-constexpr uint16_t I2C_TIMEOUT_MS = 100;
 } // namespace config
